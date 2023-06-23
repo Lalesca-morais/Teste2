74f847b26624fc15962b727c365ba840bec42e39
lalesca_moraes <lalesca.silva@zup.com.br>
2023-06-23 10:30:04 -0300

    commit inicial

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -5,23 +5,12 @@ public class Banco {
         this.nomeCliente = nomeCliente;
         this.saldo = saldoInicial;
     }
-    public void sacar(double valor) {
+    public void sacar(String valor) {
         if (valor > 0 && valor <= saldo) {
             saldo -= valor;
             System.out.println("Saque de R$" + valor + " realizado com sucesso.");
         } else {
             System.out.println("Saldo insuficiente para realizar o saque.");
         }
-    }
-    public void depositar(double valor) {
-        if (valor > 0) {
-            saldo += valor;
-            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
-        } else {
-            System.out.println("Valor inválido para depósito.");
-        }
-    }
-    public void exibirSaldo() {
-        System.out.println("Saldo atual: R$" + saldo);
     }
 }
--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Banco banco = new Banco("José", 1000.0);
-        banco.exibirSaldo();
         banco.sacar(500.0);
-        banco.exibirSaldo();
-        banco.depositar(100.0);
-        banco.exibirSaldo();
+
     }
 }
 
